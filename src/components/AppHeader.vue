690f04c95780fa4755b766effbf727237cdba407
Mxlten19 <nhutton110@gmail.com>
2026-04-13 16:43:11 -0500

    Added files for excercise 4

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -17,10 +17,13 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto">
             <li class="nav-item">
-              <RouterLink to="/" class="nav-link active">Home</RouterLink>
+              <RouterLink to="/" class="nav-link">Home</RouterLink>
             </li>
             <li class="nav-item">
               <RouterLink class="nav-link" to="/about">About</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/movies/create">Add Movie</RouterLink>
             </li>
           </ul>
         </div>
@@ -32,7 +35,3 @@
 <script setup>
 import { RouterLink } from "vue-router";
 </script>
-
-<style>
-/* Add any component specific styles here */
-</style>
