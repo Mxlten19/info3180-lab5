391d140804449f6bf2d5ddbdc4258abf31b00f5c
Mxlten19 <nhutton110@gmail.com>
2026-04-13 15:46:07 -0500

    Edit forms and models.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
-from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin12@3localhost/lab5'
+app.config['SECRET_KEY'] = 'your-secret-key'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import views
