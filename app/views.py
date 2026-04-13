36d43cf1b073ad2d71dddc029dbe81f2c817b9d9
Mxlten19 <nhutton110@gmail.com>
2026-04-13 16:35:47 -0500

    Added views.py

--- app/views.py
+++ app/views.py
@@ -5,26 +5,54 @@ Werkzeug Documentation:  https://werkzeug.palletsprojects.com/
 This file creates your application.
 """
 
-from app import app
-from flask import render_template, request, jsonify, send_file
 import os
-
-
-###
-# Routing for your application.
-###
+from app import app, db
+from flask import request, jsonify, send_from_directory
+from app.forms import MovieForm
+from app.models import Movie
+from werkzeug.utils import secure_filename
+from flask_wtf.csrf import generate_csrf
 
 @app.route('/')
 def index():
     return jsonify(message="This is the beginning of our API")
 
+@app.route('/api/v1/csrf-token', methods=['GET'])
+def get_csrf():
+    return jsonify({'csrf_token': generate_csrf()})
 
-###
-# The functions below should be applicable to all Flask apps.
-###
 
-# Here we define a function to collect form errors from Flask-WTF
-# which we can later use
+@app.route('/api/v1/movies', methods=['POST'])
+def movies():
+    form = MovieForm()
+
+    if form.validate_on_submit():
+        title = form.title.data
+        description = form.description.data
+        poster = form.poster.data
+
+        filename = secure_filename(poster.filename)
+        poster.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        movie = Movie(
+            title=title,
+            description=description,
+            poster=filename
+        )
+        db.session.add(movie)
+        db.session.commit()
+
+        return jsonify({
+            "message": "Movie Successfully added",
+            "title": title,
+            "poster": filename,
+            "description": description
+        }), 201
+
+    else:
+        errors = form_errors(form)
+        return jsonify({"errors": errors}), 400
+
 def form_errors(form):
     error_messages = []
     """Collects form errors"""
@@ -38,6 +66,7 @@ def form_errors(form):
 
     return error_messages
 
+
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
     """Send your static text file."""
@@ -47,11 +76,6 @@ def send_text_file(file_name):
 
 @app.after_request
 def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also tell the browser not to cache the rendered page. If we wanted
-    to we could change max-age to 600 seconds which would be 10 minutes.
-    """
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=0'
     return response
@@ -60,4 +84,4 @@ def add_header(response):
 @app.errorhandler(404)
 def page_not_found(error):
     """Custom 404 page."""
-    return render_template('404.html'), 404
+    return jsonify(error="Page not found"), 404
