790e319db1cbf8e1e91b3628149f3a9c0069c36d
Mxlten19 <nhutton110@gmail.com>
2026-04-13 22:29:49 -0500

    Added files for excercise 5

--- app/config.py
+++ app/config.py
@@ -4,9 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config(object):
-    """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
-    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', 'uploads')
+    UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'uploads')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:yourpassword@localhost/lab5')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
