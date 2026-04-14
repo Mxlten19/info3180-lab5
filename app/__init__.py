38e362d3809388e3139ce5cdefa3246c5990dc82
Mxlten19 <nhutton110@gmail.com>
2026-04-13 22:45:38 -0500

    Added files for excercise 5

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from flask_migrate import Migrate
 from .config import Config
 
 app = Flask(__name__)
@@ -8,5 +9,6 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
+migrate = Migrate(app, db)
 
 from app import views
