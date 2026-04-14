ea3fee36d69baa5262135af3b4e034cb0bdafef5
Mxlten19 <nhutton110@gmail.com>
2026-04-13 22:09:19 -0500

    Added files for excercise 5

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 from .config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
+csrf = CSRFProtect(app)
 
 from app import views
